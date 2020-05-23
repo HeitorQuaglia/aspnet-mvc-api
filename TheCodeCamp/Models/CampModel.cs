bd9a4d250ce79608fc5fdc9ff7fdfbd701d1f408
Heitor Quaglia <heitor.bq@hotmail.com>
2020-05-23 18:27:52 -0300

    creating CampModel

--- TheCodeCamp/Models/CampModel.cs
+++ TheCodeCamp/Models/CampModel.cs
@@ -7,5 +7,9 @@ namespace TheCodeCamp.Models
 {
     public class CampModel
     {
+        public string Name { get; set; }
+        public string Moniker { get; set; }
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        public int Length { get; set; } = 1;
     }
 }
