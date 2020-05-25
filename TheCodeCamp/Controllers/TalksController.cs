ba9c99649b40b972f0064c21f742296fffae9771
Heitor Quaglia <heitor.bq@hotmail.com>
2020-05-25 17:48:45 -0300

    add TalksController

---
+++ TheCodeCamp/Controllers/TalksController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using TheCodeCamp.Data;
+
+namespace TheCodeCamp.Controllers
+{
+    [Route("api/camps/{Moniker}/talks")]
+    public class TalksController : ApiController
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public TalksController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        // GET: Talks
+        
+    }
+}
